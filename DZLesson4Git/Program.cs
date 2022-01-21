17b9017da970cb018617d1f6360f9efb72970b14
DavidSemitotskiy <davidsemitotskiy@gmail.com>
2022-01-21 13:14:49 +0200

    initial

---
+++ DZLesson4Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZLesson4Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
